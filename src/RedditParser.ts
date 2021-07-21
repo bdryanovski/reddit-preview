cbd7559f96c2eee4f6090fc11e2976966d4aa959
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2021-07-21 16:59:24 +0300

    fix: ....
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/RedditParser.ts
+++ src/RedditParser.ts
@@ -25,63 +25,87 @@ Object.defineProperty(Array.prototype, 'chunk', {
 export class RedditParser extends LitElement {
   static styles = css`
     :host {
-      border: 1px solid #aba6a6;
-      width: calc(100% - 2rem);
-      height: 100%;
-      display: block;
-      border-radius: 5px;
-      background-color: rgb(228 228 228);
-      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+      font-family: Arial, sans-serif;
+      background-color: #fff;
+      border-radius: 10px;
+      box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
+      width: 100%;
+      overflow: hidden;
+      display: flex;
+    }
 
-      padding: 1rem;
+    * {
+      box-sizing: border-box;
+    }
+
+    .controls {
+      background-color: #2A265F;
+      color: #fff;
+      padding: 30px;
+      max-width: 350px;
+    }
+    .controls h3 {
+      opacity: 0.6;
+      margin: 0;
+      letter-spacing: 1px;
+      text-transform: uppercase;
+    }
+
+    .controls p {
+      opacity: 0.4;
+      letter-spacing: 1px;
+      white-space: pre-wrap;
+    }
+
+    .preview {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-content: space-around
     }
 
     input {
-      width: 40%;
+      width: auto;
       padding: 8px 16px;
       font-weight: 500;
       border-width: 1px;
       border-radius: 2px;
-      color: #3e64ff;
-      border-color: #3e64ff;
+      color: #2A265F;
+      border-color: #2A265F;
     }
 
     button {
-      box-sizing: border-box;
-      margin: 0;
-      overflow: visible;
       display: inline-block;
       text-align: center;
-      vertical-align: middle;
-      user-select: none;
       border: 1px solid transparent;
-      line-height: 1.5;
-      margin-bottom: 0 !important;
-      margin-right: 1rem !important;
       padding: 5px 16px;
       border-width: 1px;
       border-radius: 2px;
       font-size: 14px;
-      font-weight: 400;
       cursor: pointer;
-      background: #3e64ff;
-      border-color: #3e64ff;
+      background: #2A265F;
+      border-color: #fff;
       color: #fff;
     }
-
-    button:hover {
-      border-color: #fff;
+    .preview button {
+      width: 200px;
+      margin: 15px auto;
     }
 
+    .form {
+      display: inline-flex;
+      flex-wrap: nowrap;
+      gap: 15px;
+    }
 
     .grid {
       display: grid;
-      gap: 25px;
+      gap: 10px;
       grid-template-columns: repeat(3, 1fr);
       grid-template-rows: repeat(3, 1fr);
       margin: 0 auto 80px;
       max-width: 100vw;
-
       padding: 1rem;
     }
 
@@ -100,19 +124,14 @@ export class RedditParser extends LitElement {
       grid-row: auto / span 2;
     }
 
-    .loadmore {
-      display: block;
-      width: 100%;
-    }
-
     .nodata {
-      color: #3e64ff;
+      color: #2A265F;
+      letter-spacing: 1px;
+      margin: 1rem;
     }
   `;
 
   @property({ type: String }) user = '';
-
-  @property({ type: Number }) column = 5;
 
   posts: any = [];
 
@@ -158,34 +177,53 @@ export class RedditParser extends LitElement {
         `
     }
 
-    return html`<img src="${data.url_overridden_by_dest}" alt="${data.title}" loading=lazy >`
+    return html`<img src="${data.url_overridden_by_dest}" alt="${data.title}" loading="lazy" >`
   }
 
   loadMore() {
     this.collectData(this.user, this.next)
   }
 
+  renderPreview() {
+    if (this.posts.length === 0) {
+      return html`<h2 class="nodata">Nothing to show at the moment, try to fetch it ...</h2>`
+    }
+
+    return html`
+      <div class="grid">
+        ${this.posts
+        .filter((post: any): any => post.data.url)
+        .map((post: any) => html`
+          <div class="item">
+            <a class="link" href=${post.data.url} target="_blank">
+              ${this.embed(post.data)}
+            </a>
+          </div>`
+        )
+      }
+      </div>
+      <button @click=${this.loadMore}>Load more</button>
+    `;
+  }
+
   render() {
     return html`
-      <input id="x" type="text" placeholder="${placeHolder}" .value="${this.user}" @change=${this.onChangeInput} />
-      <button @click=${this.fetchData}>Fetch</button>
-      <div class="grid">
-        ${
-          this.posts.length === 0
-            ? html`<p class="nodata">Nothing to show you at the moment</p>`
-            : this.posts
-              .filter((post: any): any => post.data.url)
-              .map((post: any) => html`
-                <div class="item">
-                  <a class="link" href=${post.data.url} target="_blank">
-                    ${this.embed(post.data)}
-                  </a>
-                </div>`
-              )
-        }
+      <div class="controls">
+        <h3>Reddit:</h3>
+        <div class="form">
+          <input id="reddit" type="text" placeholder="${placeHolder}" .value="${this.user}" @change=${this.onChangeInput} />
+          <button @click=${this.fetchData}>Fetch</button>
+        </div>
+        <p>
+          Try to fetch and display images from Reddit, why don't you try typing r/meme and check it for yourself.
+        </p>
       </div>
 
-      ${ this.posts.length ? html`<center><button class="loadmore" @click=${this.loadMore}>Load more</button></center>` : ''}
+      <div class="preview">
+        ${
+          this.renderPreview()
+        }
+      </div>
      `;
   }
 }
